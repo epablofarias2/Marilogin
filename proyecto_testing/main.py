c3f0bd2e7cb4e1f220e023864d385ff78168a811
unknown <franciscomarinoni36@gmail.com>
2023-09-22 19:55:02 -0300

    version 1.0

---
+++ proyecto_testing/main.py
@@ -0,0 +1,99 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from utils import util1
+
+app = Flask(__name__)
+
+# Rutas de la aplicación
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/registrarse')
+def registrarse():
+    return render_template('registrarse.html')
+
+@app.route('/cambiar_contraseña')
+def cambiar_contraseña():
+    return render_template('cambiar_contraseña.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+#Traemos los datos dle usuario, los comprobamos y los enviamos a la base de datos
+@app.route('/registrarse', methods=['POST'])
+def registrarse_post():
+    username = request.form['username']
+    password = request.form['password']
+    password2 = request.form['password2']
+    pregunta_seguridad = request.form['pregunta_seguridad']
+    
+    if password != password2:
+        return render_template('registrarse.html', error='Las contraseñas no coinciden')
+
+    if not util1.requisitos_contraseña(password):
+        return render_template('registrarse.html', error="La contraseña debe tener 8 o más caracteres, una mayúscula y una minúscula")
+    
+    if util1.verificar_usuario(username):
+        return render_template('registrarse.html', error='El usuario ya existe')
+    
+    util1.enviar_datos(username, password, pregunta_seguridad)
+    return redirect(url_for('index')) 
+
+#Vereficamos el inicio de sesion
+@app.route("/iniciar_sesion", methods=['POST'])
+def iniciar_sesion_post():
+    username = request.form['username']
+    password = request.form['password']
+
+    if not util1.verificar_usuario(username):
+        return render_template('index.html', error='El usuario no existe')
+    elif not util1.verificar_contraseña(username, password):
+        return render_template('index.html', error='Contraseña incorrecta')
+    else: 
+        return redirect(url_for('home'))
+
+#Verificamos si la pregunta de seguridad esta bien 
+@app.route("/cambiar_contraseña", methods=['POST'])
+def pregunta_seguridad_post():
+    username = request.form['username']
+    pregunta = request.form['pregunta_seguridad']
+    nueva_password = request.form['npassword']
+
+    if util1.verificar_respuesta(username, pregunta):
+        util1.reemplazar_contraseña(username, nueva_password)
+        return redirect(url_for('index'))
+    else:
+        return render_template('cambiar_contraseña.html', error='La respuesta de seguridad es incorrecta')
+
+
+# Conectarse a la base de datos (si no existe, se crea)
+conexion = sqlite3.connect('proyecto_testing\\data\\usuarios.db')
+cursor = conexion.cursor()
+
+# Define la sentencia SQL para crear la tabla
+tabla_sql = '''
+CREATE TABLE IF NOT EXISTS mi_tabla (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT,
+    password TEXT,
+    pregunta_seguridad TEXT
+)
+'''
+
+# Ejecutar la sentencia SQL para crear la tabla
+cursor.execute(tabla_sql)
+
+# Guardar los cambios y cerrar la conexión a la base de datos
+conexion.commit()
+conexion.close()
+
+print("La tabla se ha creado exitosamente.")
+
+"""
+Solo por hacer esto nos merecemos un 10!!!!!
+"""
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ proyecto_testing/utils/util1.py
@@ -0,0 +1,86 @@
+import sqlite3
+
+def requisitos_contraseña(contraseña):
+    """
+    Verifica que la contraseña cumpla con los siguientes requisitos:
+    """
+    longitud_valida = len(contraseña) >= 8
+    contiene_mayuscula = any(c.isupper() for c in contraseña)
+    contiene_minuscula = any(c.islower() for c in contraseña)
+    contiene_numero = any(caracter.isdigit() for caracter in contraseña)
+
+    if not longitud_valida:
+        return False
+    elif not contiene_mayuscula:
+        return False
+    elif not contiene_minuscula:
+        return False
+    elif not contiene_numero:
+        return False
+    else:
+        return True
+
+def enviar_datos(username, password, pregunta_seguridad):
+    """ 
+    Envía los datos del usuario a la base de datos
+    """ 
+    conexion = sqlite3.connect('proyecto_testing/data/usuarios.db')
+    cursor = conexion.cursor()
+    cursor.execute("INSERT INTO mi_tabla (username, password, pregunta_seguridad) VALUES (?, ?, ?)", (username, password, pregunta_seguridad))
+    conexion.commit()
+    conexion.close()
+
+def verificar_usuario(username):
+    """
+    Verifica que el usuario exista en la base de datos
+    """
+    conexion = sqlite3.connect('proyecto_testing/data/usuarios.db')
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM mi_tabla WHERE username=?", (username,))
+    usuario = cursor.fetchone()
+    conexion.close()
+    if usuario is None:
+        return False
+    else:
+        return True
+
+def verificar_contraseña(username, password):   
+    """
+    Verifica que la contraseña sea correcta
+    """
+    conexion = sqlite3.connect('proyecto_testing/data/usuarios.db')
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM mi_tabla WHERE username=? AND password=?", (username, password))
+    usuario = cursor.fetchone()
+    conexion.close()
+    if usuario is None:
+        return False
+    else:
+        return True
+
+def verificar_respuesta (username, pregunta):
+    """
+    Verifica que la respues de seguridad sea la correcta
+    """
+    conexion = sqlite3.connect('proyecto_testing/data/usuarios.db')
+    cursor = conexion.cursor()
+    cursor.execute("SELECT * FROM mi_tabla WHERE username=? AND pregunta_seguridad=?", (username, pregunta))
+    usuario = cursor.fetchone()
+    conexion.close()
+    if usuario is None:
+        return False
+    else:
+        return True
+
+def reemplazar_contraseña(usuario_id, new_password):
+    """
+    Sirve para reemplazar la contraseña dentro de la base de datos  
+    """
+    conexion = sqlite3.connect('proyecto_testing/data/usuarios.db')
+    cursor = conexion.cursor()
+    
+    consulta_actualizar_contraseña = "UPDATE mi_tabla SET password = ? WHERE id = ?"
+
+    cursor.execute(consulta_actualizar_contraseña, (new_password, usuario_id))
+    conexion.commit()
+    conexion.close()
